85d9b54f0f6bc9b11be4a02e8a7e6c62a0abdc6e
Mikhail_Zakirov <mikhailzakirov1@gmail.com>
2022-11-17 16:40:35 +0300

    Создан репозиторий добавлены примеры работы с деревом

--- lab11/src/main/java/ru/itis/lab11/Main.java
+++ lab11/src/main/java/ru/itis/lab11/Main.java
@@ -2,6 +2,8 @@ package ru.itis.lab11;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        TreeMaker tree = new TreeMaker();
+        tree.createTree(new int[]{1,2,3,4,5,6,7,8,9});
+        tree.printTree();
     }
 }
